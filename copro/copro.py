0a33606d9bd0bb4ee547f68634ef34cf318830f3
Ian Dirk Fichtner <ifichtner@protonmail.com>
2025-05-22 11:09:14 +0200

    copro: cluster_peptides()

---
+++ copro/copro.py
@@ -0,0 +1,45 @@
+from sklearn.cluster import AgglomerativeClustering
+
+def cluster_peptides(df,
+                     column_map: dict = {'peptide_a': 'pepA',
+                                         'peptide_b': 'pepB',
+                                         'correlation_values': 'PCC',
+                                         'protein_id': 'protein_id'},
+                     method: str = 'agglomerative-hierarchical-clustering') -> dict:
+    '''
+    Perform peptide clustering grouped by protein annotation.
+
+
+    Parameters:
+    ----------
+    df : pandas.DataFrame
+        Data frame with pairwise correlations annotated with the protein they belong to.]
+    column_map: dict
+
+    method : str
+        Which clustering method to apply.
+
+    Returns:
+    -------
+    dict
+        Dictionary with clustering method output.
+        - 'agglomerative-hierarchical-clustering' => {protein_id: {'labels': list, 'height': list, 'merge': list(list)}}
+    '''
+
+    assert all(df.index == df.columns)
+
+    model = AgglomerativeClustering(n_clusters=None,
+                                    metric='precomputed',
+                                    linkage='average',
+                                    distance_threshold=0,
+                                    compute_distances=True)
+
+    model.fit(df)
+
+    dendogram = {
+        'labels': model.feature_names_in_.tolist(),
+        'height': model.distances_.tolist(),
+        'merge': model.children_.tolist()
+    }
+
+    return dendogram

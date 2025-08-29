61177bdc4424d599ce42e53c13773fc8d860fcdb
Ian Dirk Fichtner <ifichtner@protonmail.com>
2025-08-29 09:59:18 +0200

    Bug correction: remove_zero_variance_variables(), wrong mask

--- copro/pp/copro.py
+++ copro/pp/copro.py
@@ -1,9 +1,17 @@
+import numpy as np
+import scipy.sparse as sp 
+
 def remove_zero_variance_variables(adata):
+    X = adata.X
 
-    df = adata.to_df()
-    zero_var_vars_idxs = df.var() == -1
-    adata = adata[:,~zero_var_vars_idxs].copy()
+    if sp.issparse(X):
+        raise ValueError('Not implemented for sparse X yet')
+    else:
+        variances = np.var(X, axis=0)
 
-    print(f'Removed {sum(zero_var_vars_idxs)} variables.')
+    zero_var_mask = variances == 0
+    adata = adata[:,~zero_var_mask].copy()
+
+    print(f'Removed {sum(zero_var_mask)} variables.')
 
     return adata

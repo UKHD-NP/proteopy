043228f91eb03cec893989dfba4473490587dc08
Ian Dirk Fichtner <ifichtner@protonmail.com>
2025-08-31 12:20:52 +0200

    Added pl.proteoform_intensities(), wrapper of pl.peptide_intensities(); removed previous proteoform plotting function

--- copro/pl/__init__.py
+++ copro/pl/__init__.py
@@ -1,3 +1,3 @@
-from .proteoforms import proteoforms, proteoform_scores
-from .peptides import peptide_intensities, n_peptides_per_gene
 from .obs import n_samples_by_category
+from .peptides import peptide_intensities, n_peptides_per_gene
+from .proteoforms import proteoform_scores, proteoform_intensities

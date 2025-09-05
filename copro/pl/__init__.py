fb136234eb156848495accb784ea19f0870b23cb
Ian Dirk Fichtner <ifichtner@protonmail.com>
2025-09-05 10:51:10 +0200

    Corrected pl.__init__()

--- copro/pl/__init__.py
+++ copro/pl/__init__.py
@@ -1,3 +1,3 @@
 from .obs import n_samples_by_category
-from .peptides import peptide_intensities, n_peptides_per_gene
-from .proteoforms import proteoform_scores, proteoform_intensities
+from .intensities import peptide_intensities, n_peptides_per_gene, proteoform_intensities
+from .proteoforms import proteoform_scores

f5a67d31c07706df77b634839f8f1e12bf19cd84
ibludau <isabell.bludau@gmail.com>
2025-09-24 10:49:43 +0200

    feature: median normalization

--- copro/pp/__init__.py
+++ copro/pp/__init__.py
@@ -2,6 +2,7 @@ from .obs import filter_category_count
 from .var import (
     filter_var_completeness,
     is_log_transformed,
+    median_normalize,
 )
 from .peptides import filter_genes_by_peptide_count
 from .copro import remove_zero_variance_variables

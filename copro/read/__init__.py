8696d51950129898a662f07dd39b6a216b5cdfdc
Ian Dirk Fichtner <ifichtner@protonmail.com>
2025-10-24 16:57:16 +0200

    Make read.peptides/proteins_long_from_df() public API

--- copro/read/__init__.py
+++ copro/read/__init__.py
@@ -1,3 +1,9 @@
-from .peptides_generic import peptides_long
-from .proteins_generic import proteins_long
+from .peptides_generic import (
+    peptides_long,
+    peptides_long_from_df,
+    )
+from .proteins_generic import (
+    proteins_long,
+    protins_long_from_df,
+    )
 from .diann import diann

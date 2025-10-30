b811825ccf455d896beb0435aa2e83ecac8b024f
Ian Dirk Fichtner <ifichtner@protonmail.com>
2025-10-30 22:30:37 +0100

    Refactor: tl/ filenames

--- copro/tl/__init__.py
+++ copro/tl/__init__.py
@@ -1,7 +1,6 @@
-from .copro import (
+from .copf import (
     pairwise_peptide_correlations,
     peptide_dendograms_by_correlation,
     peptide_clusters_from_dendograms,
     proteoform_scores,
     )
-from .quantify import quantify_by_var
